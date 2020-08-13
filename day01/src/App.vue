5cc301d294184fdb30a160a6d958b66aca5c7ec9
liyirong <1504091097@qq.com>
2020-08-13 09:47:44 +0800

    第一天代码提交

---
+++ day01/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <!-- 只要标签中间没有写代码就都用单标签 -->
+      <Header/>
+      <router-view></router-view>
+      <!-- 判断页面下面是否要有footer -->
+      <Footer v-if="!$route.meta.isHide"/>
+      <!-- <Footer v-if="$route.path ==='/home' || $route.path === '/search'"/> -->
+      <!-- v-if="$route.path ==='/home' || $route.path === '/search'" -->
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default {
+  name: 'App',
+  
+  components:{
+    Header,
+    Footer
+  }
+
+}
+</script>
+
+<style>
+</style>
---
+++ day01/src/pages/Search/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Search <br>
+    params参数:{{$route.params.keyword}}
+    params参数简便获取:{{keyword}}
+    query参数:{{$route.query.keyword}}
+    query参数简便获取:{{keyword2}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+   props:['keyword','keyword2','name']
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
